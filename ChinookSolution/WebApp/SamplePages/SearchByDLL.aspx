9b63f8c50981083b8b27d94516c4757c42b37ade
spopik2 <70964549+spopik2@users.noreply.github.com>
2021-01-15 16:08:42 -0700

    Created LoadArtist Method in code behind

--- ChinookSolution/WebApp/SamplePages/SearchByDLL.aspx
+++ ChinookSolution/WebApp/SamplePages/SearchByDLL.aspx
@@ -1,3 +1,12 @@
-﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="SearchByDLL.aspx.cs" Inherits="WebApp.SamplePages.SearchByDLL" %>
+﻿<%@ Page Title="Filter Search Demo" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="SearchByDLL.aspx.cs" Inherits="WebApp.SamplePages.SearchByDLL" %>
 <asp:Content ID="Content1" ContentPlaceHolderID="MainContent" runat="server">
+    <h1>Search Albums by Artist</h1>
+    <%-- search area --%>
+    <div class="row">
+        <div class="offset-3">
+            <asp:Label ID="Label1" runat="server" Text="Select an artist:"></asp:Label>&nbsp;&nbsp;
+            <asp:DropDownList ID="ArtistList" runat="server"></asp:DropDownList>&nbsp;&nbsp;
+            <asp:LinkButton ID="SearchAlbums" runat="server">&nbsp;Search</asp:LinkButton>
+        </div>
+    </div>
 </asp:Content>
--- ChinookSolution/WebApp/SamplePages/SearchByDLL.aspx.cs
+++ ChinookSolution/WebApp/SamplePages/SearchByDLL.aspx.cs
@@ -5,13 +5,38 @@ using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 
+#region Additional Namespaces
+using ChinookSystem.BLL;
+using ChinookSystem.ViewModel;
+#endregion
+
 namespace WebApp.SamplePages
 {
     public partial class SearchByDLL : System.Web.UI.Page
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+            if (!Page.IsPostBack)
+            {
+                LoadArtistList();
+            }
+        }
+        protected void LoadArtistList()
+        {
+            ArtistController sysmgr = new ArtistController();
+            List<SelectionList> info = sysmgr.Artists_DDLList();
 
+            //Lets assume the data collection needs to be sorted
+            info.Sort((x,y) => x.DisplayField.CompareTo(y.DisplayField));
+
+            //set up the DDL
+            ArtistList.DataSource = info;
+            //ArtistList.DataTextField = "DisplayField";
+            ArtistList.DataTextField = nameof(SelectionList.DisplayField);
+            ArtistList.DataValueField = nameof(SelectionList.ValueField);
+            ArtistList.DataBind();
+
+            ArtistList.Items.Insert(0, new ListItem ("Select ...", "0"));
         }
     }
 }
