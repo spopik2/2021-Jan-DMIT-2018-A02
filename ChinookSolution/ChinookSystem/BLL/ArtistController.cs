b7ddaf8e9c8a2f7ce3a0809092552d0a6d341aa7
spopik2 <70964549+spopik2@users.noreply.github.com>
2021-01-15 15:20:28 -0700

    Created ArtistController

--- ChinookSolution/ChinookSystem/BLL/ArtistController.cs
+++ ChinookSolution/ChinookSystem/BLL/ArtistController.cs
@@ -16,6 +16,19 @@ namespace ChinookSystem.BLL
     [DataObject]
     public class ArtistController
     {
-
+        [DataObjectMethod(DataObjectMethodType.Select, false)]
+        public List<SelectionList> Artists_DDLList()
+        {
+            using (var context = new ChinookSystemContext())
+            {
+                IEnumerable<SelectionList> results = from x in context.Artists
+                                                     select new SelectionList 
+                                                     {
+                                                        ValueField = x.ArtistID,
+                                                        DisplayField = x.Name
+                                                     };
+                return results.ToList();
+            }
+        }
     }
 }
